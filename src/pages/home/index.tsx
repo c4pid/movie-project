32e918a11030c1274e7f5cbb98b51d368bb9c670
khanhxoan <khanhxoan99@gmail.com>
2022-12-14 13:41:14 +0700

    update css

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
-import { Carousel } from "react-bootstrap";
+import { Button, Carousel } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
@@ -8,7 +8,7 @@ import { AppState } from "../../services/reducers";
 
 const Home = () => {
     const [listData, setListData] = useState([]);
-    const dispatch = useDispatch(); 
+    const dispatch = useDispatch();
     const listBanner = useSelector((state: AppState) => state.listMovies.listBanner);
     useEffect(() => {
         dispatch(getListBanner());
@@ -23,13 +23,17 @@ const Home = () => {
     return (
         <div className="mv-home-container">
             <div className="mv-home-banner">
-                <Carousel style={{height: '100%'}}>
+                <Carousel style={{ height: "100%" }}>
                     {listData.map((item: any) => (
                         <Carousel.Item key={item.maBanner}>
                             <img className="d-block w-100" src={item.hinhAnh} alt="First slide" />
                         </Carousel.Item>
                     ))}
                 </Carousel>
+                <div className="mv-home-btn-container">
+                    <Button variant="primary">Book ticket</Button>
+                    <Button variant="primary">More Info</Button>
+                </div>
             </div>
             <div className="mv-home-list"></div>
         </div>
